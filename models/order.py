8072d4d0a71d04d3a76092a0bc38c1a6a55555d3
sreadytocode <sara.younis2303@gmail.com>
2022-09-28 22:38:49 +0100

    lab work and homework

---
+++ models/order.py
@@ -0,0 +1,9 @@
+class Order():
+
+    def __init__(self, customer_name, order_date, quantity, donut_name, donut_topping, description):
+        self.customer_name = customer_name
+        self.order_date = order_date
+        self.quantity = quantity
+        self.donut_name = donut_name
+        self.donut_topping = donut_topping
+        self.donut_description = description
---
+++ models/order_list.py
@@ -0,0 +1,8 @@
+from models.order import *
+
+
+
+order1 = Order("Customer: Sara", "Order date: Today", 3, "Donuts: Honey Dip with ", "Chocolate Sprinkles", "Description: The best donuts ever")
+order2 = Order("Customer: Takako", "Order date: Today", 5, "Donuts: Chocolate Ring with", "Rainbow Sprinkles", "Description: Yummy")
+orders = [order1, order2]
+
